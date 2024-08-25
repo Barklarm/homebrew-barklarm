e64244ebbbabd4311d0f4b7499db46453b0b0057
Alvaro Lorete <contact@alvarolorente.dev>
2024-08-25 21:19:42 +0200

    Update barklarm to 2.3.1

--- Casks/barklarm.rb
+++ Casks/barklarm.rb
@@ -1,5 +1,5 @@
 cask "barklarm" do
-  version "2.3.0"
+  version "2.3.1"
   sha256 :no_check
 
   url "https://github.com/Barklarm/barklarm-app/releases/download/v#{version}/barklarm-#{version}-arm64.dmg",
